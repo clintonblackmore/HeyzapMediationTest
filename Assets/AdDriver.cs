12ff3d4a9d0c246d8fa7b146d536aad2a8e1c21b
Clinton Blackmore <clinton.blackmore@gmail.com>
2016-06-08 16:18:45 -0600

    Added code from real project; app crashes

---
+++ Assets/AdDriver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Heyzap;
+
+// Click a button, and use this script to show an ad
+
+public class AdDriver : MonoBehaviour 
+{
+    public void ShowAd(string placementName)
+    {
+        AdCentral.ShowAd(placementName);
+    }
+
+    public void ShowMediationSetup()
+    {
+        HeyzapAds.ShowMediationTestSuite();
+    }
+}
